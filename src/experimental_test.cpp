fc7a49c7e5bc4352734399b43fe56d72fedb582d
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2019-04-18 18:25:29 +0200

    Started working on a "tagged tuple" implem to be used to generate structs at compile-time

--- src/experimental_test.cpp
+++ src/experimental_test.cpp
@@ -25,5 +25,60 @@ The fact that you are presently reading this means that you have had knowledge o
 license and that you accept its terms.*/
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-
 #include "doctest.h"
+
+#include <string>
+#include <tuple>
+
+template <class Tag, class Type>
+struct field {};
+
+// tag -> index correspondance
+template <int>
+auto helper() {
+    return std::tuple<>();
+}
+
+template <int index, class Tag, class Type, class... Rest>
+auto helper(field<Tag, Type>, Rest... rest) {
+    return std::tuple_cat(std::make_tuple(Tag()), helper<index + 1>(rest...));
+}
+
+template <class... Fields>
+auto make_named_tuple_type(Fields... fields) {
+    return helper<0>(fields...);
+}
+
+// INDEXES
+template <int, class Tag>
+auto index_helper(std::tuple<>) {
+    return std::integral_constant<int, -1>();
+}
+
+template <int index, class Tag, class First, class... Rest>
+auto index_helper(std::tuple<First, Rest...>) {
+    using t_if_same = std::integral_constant<int, index>;
+    using t_if_different = decltype(index_helper<index + 1, Tag>(std::tuple<Rest...>()));
+    using same_type = std::is_same<Tag, First>;
+    return std::conditional_t<same_type::value, t_if_same, t_if_different>();
+}
+
+template <class Tag, class... Tags>
+constexpr auto get_index(std::tuple<Tags...> tags) {
+    using result = decltype(index_helper<0, Tag>(tags));
+    static_assert(result::value != -1, "Tag not found");
+    return result::value;
+}
+
+TEST_CASE("Basic tuple test") {
+    struct alpha {};
+    struct beta {};
+    struct gamma {};
+
+    using tlist = std::tuple<alpha, beta>;
+    CHECK(get_index<alpha>(tlist()) == 0);
+    CHECK(get_index<beta>(tlist()) == 1);
+    // CHECK(get_index<gamma>(tlist()) == -1); // triggers static assert
+
+    // auto t = make_named_tuple_type(field<alpha, int>(), field<beta, std::string>());
+}
