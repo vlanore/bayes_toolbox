db6ed942741ab92c26bd45b46351f4b4c923aa82
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2019-04-23 15:03:38 +0200

    Moved ttuple under-the-hood things into a helper namespace

--- src/experimental_test.cpp
+++ src/experimental_test.cpp
@@ -36,72 +36,73 @@ using std::tuple;
 template <class Tag, class Type>
 struct field {};
 
-// tag -> index correspondance
-auto helper(std::tuple<>) { return std::tuple<>(); }
+namespace ttuple_helper {  // tag -> index correspondance
+    auto helper(std::tuple<>) { return std::tuple<>(); }
 
-template <class Tag, class Type, class... Rest>
-auto helper(tuple<field<Tag, Type>, Rest...>) {
-    return std::tuple_cat(std::make_tuple(Tag()), helper(tuple<Rest...>()));
-}
+    template <class Tag, class Type, class... Rest>
+    auto helper(tuple<field<Tag, Type>, Rest...>) {
+        return std::tuple_cat(std::make_tuple(Tag()), helper(tuple<Rest...>()));
+    }
 
-template <class... Fields>
-auto get_tags(tuple<Fields...> fields) {
-    return helper(fields);
-}
+    template <class... Fields>
+    auto get_tags(tuple<Fields...> fields) {
+        return helper(fields);
+    }
 
-auto tuple_helper(std::tuple<>) { return std::tuple<>(); }
+    auto tuple_helper(std::tuple<>) { return std::tuple<>(); }
 
-template <class Tag, class Type, class... Rest>
-auto tuple_helper(tuple<field<Tag, Type>, Rest...>) {
-    return std::tuple_cat(std::make_tuple(Type()), tuple_helper(tuple<Rest...>()));
-}
+    template <class Tag, class Type, class... Rest>
+    auto tuple_helper(tuple<field<Tag, Type>, Rest...>) {
+        return std::tuple_cat(std::make_tuple(Type()), tuple_helper(tuple<Rest...>()));
+    }
 
-template <class... Fields>
-auto get_tuple(tuple<Fields...> fields) {
-    return tuple_helper(fields);
-}
+    template <class... Fields>
+    auto get_tuple(tuple<Fields...> fields) {
+        return tuple_helper(fields);
+    }
 
-// INDEXES
-template <int, class Tag>
-auto index_helper(std::tuple<>) {
-    return std::integral_constant<int, -1>();
-}
+    // INDEXES
+    template <int, class Tag>
+    auto index_helper(std::tuple<>) {
+        return std::integral_constant<int, -1>();
+    }
 
-template <int index, class Tag, class First, class... Rest>
-auto index_helper(std::tuple<First, Rest...>) {
-    using t_if_same = std::integral_constant<int, index>;
-    using t_if_different = decltype(index_helper<index + 1, Tag>(std::tuple<Rest...>()));
-    using same_type = std::is_same<Tag, First>;
-    return std::conditional_t<same_type::value, t_if_same, t_if_different>();
-}
+    template <int index, class Tag, class First, class... Rest>
+    auto index_helper(std::tuple<First, Rest...>) {
+        using t_if_same = std::integral_constant<int, index>;
+        using t_if_different = decltype(index_helper<index + 1, Tag>(std::tuple<Rest...>()));
+        using same_type = std::is_same<Tag, First>;
+        return std::conditional_t<same_type::value, t_if_same, t_if_different>();
+    }
 
-template <class Tag, class... Tags>
-constexpr auto get_index(std::tuple<Tags...> tags) {
-    using result = decltype(index_helper<0, Tag>(tags));
-    static_assert(result::value != -1, "Tag not found");
-    return result::value;
-}
+    template <class Tag, class... Tags>
+    constexpr auto get_index(std::tuple<Tags...> tags) {
+        using result = decltype(index_helper<0, Tag>(tags));
+        static_assert(result::value != -1, "Tag not found");
+        return result::value;
+    }
 
-template <class Tags, class Tuple>
-struct tagged_tuple {
-    Tuple data;
-    template <class... Args>
-    tagged_tuple(Args&&... args) : data(std::forward<Args>(args)...) {}
-};
+    template <class Tags, class Tuple>
+    struct tagged_tuple {
+        Tuple data;
+        template <class... Args>
+        tagged_tuple(Args&&... args) : data(std::forward<Args>(args)...) {}
+    };
+
+    template <class Fields>
+    auto make_tagged_tuple_type() {
+        using tags = decltype(get_tags(Fields()));
+        using data = decltype(get_tuple(Fields()));
+        return tagged_tuple<tags, data>();
+    }
+};  // namespace ttuple_helper
 
 template <class Fields>
-auto make_tagged_tuple_type() {
-    using tags = decltype(get_tags(Fields()));
-    using data = decltype(get_tuple(Fields()));
-    return tagged_tuple<tags, data>();
-}
-
-template <class Fields>
-using ttuple = decltype(make_tagged_tuple_type<Fields>());
+using ttuple = decltype(ttuple_helper::make_tagged_tuple_type<Fields>());
 
 template <class Tag, class Tags, class Tuple>
-auto get(const tagged_tuple<Tags, Tuple>& ttuple) {
-    return get<get_index<Tag>(Tags())>(ttuple.data);
+auto get(const ttuple_helper::tagged_tuple<Tags, Tuple>& ttuple) {
+    return get<ttuple_helper::get_index<Tag>(Tags())>(ttuple.data);
 }
 
 TEST_CASE("Basic tuple test") {
@@ -110,22 +111,25 @@ TEST_CASE("Basic tuple test") {
     struct gamma {};
 
     using my_fields = std::tuple<field<alpha, int>, field<beta, std::string>>;
-    using my_tags = decltype(get_tags(my_fields()));
-    using my_tuple_t = decltype(get_tuple(my_fields()));
+    {
+        using namespace ttuple_helper;
+        using my_tags = decltype(get_tags(my_fields()));
+        using my_tuple_t = decltype(get_tuple(my_fields()));
 
-    my_tuple_t my_tuple{2, "hello"};
+        my_tuple_t my_tuple{2, "hello"};
 
-    using tlist = std::tuple<alpha, beta>;
-    bool ok_tags = std::is_same<my_tags, tlist>::value;
-    CHECK(ok_tags);
-    CHECK(get_index<alpha>(tlist()) == 0);
-    CHECK(get_index<beta>(tlist()) == 1);
-    // CHECK(get_index<gamma>(tlist()) == -1); // triggers static assert
+        using tlist = std::tuple<alpha, beta>;
+        bool ok_tags = std::is_same<my_tags, tlist>::value;
+        CHECK(ok_tags);
+        CHECK(get_index<alpha>(tlist()) == 0);
+        CHECK(get_index<beta>(tlist()) == 1);
+        // CHECK(get_index<gamma>(tlist()) == -1); // triggers static assert
 
-    auto a = get<get_index<beta>(tlist())>(my_tuple);
-    CHECK(a == "hello");
-    auto b = get<get_index<alpha>(tlist())>(my_tuple);
-    CHECK(b == 2);
+        auto a = get<get_index<beta>(tlist())>(my_tuple);
+        CHECK(a == "hello");
+        auto b = get<get_index<alpha>(tlist())>(my_tuple);
+        CHECK(b == 2);
+    }
 
     using hello_t = ttuple<my_fields>;
     hello_t my_other_struct{3, "hi"};
