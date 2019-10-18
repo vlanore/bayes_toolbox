5f7f12365010a6bf6c14534082c241582036ae14
Hugo menet <menet.hugo@gmail.com>
2019-10-18 17:43:25 +0200

    unif bern fini

--- examples/unif_bern.cpp
+++ examples/unif_bern.cpp
@@ -1,27 +1,36 @@
-//p unif(0,1)
-//b bern(p)
+// p unif(0,1)
+// b bern(p)
 
-//fixer b à 1, et normalement 2/3
+// fixer b à 1, et normalement 2/3
 
 #include <iostream>
 #include "basic_moves.hpp"
+#include "distributions/bernoulli.hpp"
+#include "distributions/uniform.hpp"
 #include "mcmc_utils.hpp"
 #include "operations/backup.hpp"
 #include "operations/logprob.hpp"
 #include "operations/raw_value.hpp"
-#include "distributions/bernoulli.hpp"
-#include "distributions/uniform.hpp"
 using namespace std;
 
+int main() {
+    auto gen = make_generator();
 
-int main(){
-	
-	auto gen = make_generator();
-	
-	auto a = make_node<uniform>(0,1);
-	auto b = make_node<bernoulli>(a);
-	
-	get<value>(b) = 1;
-	draw(a,gen);
-	cout<<"a="<<get<value>(a)<<"\n";
+    auto a = make_node<uniform>(0, 1);
+    auto b = make_node<bernoulli>(a);
+
+    get<value>(b) = 1;
+    draw(a, gen);
+    cout << "a=" << get<value>(a) << "\n";
+    double sum_a = 0;
+
+    for (int i = 0; i < 10'000; i++) {
+        auto bkp = backup(a);
+        double log_prob_before = logprob(a) + logprob(b);
+        double log_hastings = slide_constrained(get<value>(a), 0, 1, gen);
+        bool accept = decide(logprob(a) + logprob(b) - log_prob_before + log_hastings, gen);
+        if (!accept) { restore(a, bkp); }
+        sum_a += get<value>(a);
+    }
+    cout << "a=" << sum_a / 10'000. << "\n";
 }
