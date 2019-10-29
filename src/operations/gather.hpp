2cb454f8e008d1e7887239f15d28302f39e4f526
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2019-10-29 17:43:38 +0100

    Fixed gather expecting copies of dnode parameters

--- src/operations/gather.hpp
+++ src/operations/gather.hpp
@@ -31,7 +31,7 @@ license and that you accept its terms.*/
 #include "structure/type_tag.hpp"
 template <class T>
 void gather(T& x)   {
-    auto gather_dnode = [](auto detfunction, auto& x, auto... params) {
+    auto gather_dnode = [](auto detfunction, auto& x, auto&&... params) {
         decltype(detfunction)::gather(x, params...);
     };
     across_nodes(x, gather_dnode);
